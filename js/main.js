1c5328a10c38602597569544a3b21ea2b439a081
Julio Cesar Barreto Nogueira <cesarsco15@gmail.com>
2020-03-21 13:31:21 -0300

    UpComentarios

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-
-function busca(valor){
+function busca(valor) {
     const procura = document.querySelector('#search')
     procura.value = ""
     const serviceUrl = `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${valor}`
     axios.defaults.baseURL = 'http://Dominio';
-    axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
+    axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
     axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
     axios.defaults.headers['x-rapidapi-host'] = 'imdb-internet-movie-database-unofficial.p.rapidapi.com'
     axios.defaults.headers['x-rapidapi-key'] = 'f72a6b1673mshb4b0130bc4010c3p1eed21jsn3a5f09b4d080'
@@ -13,25 +12,25 @@ function busca(valor){
             const dados = resp.data.titles
             const lista = Array.from(dados)
             // delete lista[`${lista.length - 1}`]
-            exibe(lista)           
+            exibe(lista)
         })
         .catch(error => {
-        console.log(error);
-        })    
+            console.log(error);
+        })
 }
 
-function click(){
-    const procura = document.querySelector('#search')    
-    busca(procura.value)    
+function click() {
+    const procura = document.querySelector('#search')
+    busca(procura.value)
 }
 
-function exibe(lista){
+function exibe(lista) {
     lista.map(e => {
         perfil(e)
     })
 }
 
-function perfil(e){
+function perfil(e) {
     const doc = document.querySelector('#collection')
     const div = document.createElement('div')
     const img = document.createElement('img')
@@ -43,11 +42,11 @@ function perfil(e){
     h3.innerHTML = e.title
     h3.classList = "tituloF"
     div.append(h3)
-    doc.append(div)   
+    doc.append(div)
 }
 
 const busco = document.querySelector('#buscou')
-busco.onclick = function(){
+busco.onclick = function () {
     const doc = document.querySelector('#collection')
     doc.innerHTML = ""
     click()
--- js/slide.js
+++ js/slide.js
@@ -1,31 +1,45 @@
-function slide(){
-    let img1 = document.querySelectorAll('.img')
-    const listaMov = Array.from(img1)
-    
-    listaMov.map((e , index) => {
+function slide() {
+    let img1 = document.querySelectorAll('.img') // criado uma node list 
+    const listaMov = Array.from(img1) // criado um Array para trabalhar
+
+    listaMov.map((e, index) => {
+        //map percore os elementos de um Array, representado por 'e', INDEX é posição dela startando do 0
         e.style.left = `${450 * index}px`
+        /*
+        a cada imagem que for adicionada vai receber o valor de +450px na lateral 
+        para ficar do lado e não subir uma em cima da outra 
+        */
     })
-    
-    function mouse(img){
-        const elemento = img.querySelector('.exb')  
-        img.onmouseover = function(){
+
+    function mouse(img) {
+        //seleciona uma div == img, dentro dessa div tem um elemento de texto com display none
+        const elemento = img.querySelector('.exb')
+        //selecionando o elemento dentro da div
+        img.onmouseover = function () {
             elemento.style.display = 'block'
-        }          
-        img.onmouseout = function(){
-            elemento.style.display = 'none'       
+            //passar o mouse na div ele fica visivel
+        }
+        img.onmouseout = function () {
+            elemento.style.display = 'none'
+            //retirado o mouse ele fica invisivel denovo
         }
     }
-    function mover(){   
-        listaMov.map(e => {            
+
+    function mover() {
+        listaMov.map(e => {
+            // percorendo a lista de imagens no slide e adicionando os efeitos           
             movendo(e)
+            // efeito de movimentação para todos os elementos se moverem juntos
             mouse(e)
+            // adicionado o efeito do display para cada imagem 
         })
     }
-    setInterval(mover,10)
-    function movendo(img){
-        let number = parseInt(img.style.left)       
-        img.style.left = (number < -450)? `${(listaMov.length - 1)*450}px`:`${number-3}px`
-    }    
+    setInterval(mover, 10) // isso faz com que execute o movimento da imagem a cada 10 milisegundos
+    function movendo(img) {
+        let number = parseInt(img.style.left) // pego a posição atual da imagem
+        img.style.left = (number < -450) ? `${(listaMov.length - 1)*450}px` : `${number-3}px`
+        //verifico se ela ja saiu do limite de visualização e adiciono uma nova posição
+    }
     mover()
-}  
+}
 slide()
